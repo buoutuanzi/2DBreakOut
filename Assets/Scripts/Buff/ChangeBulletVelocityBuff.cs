e80243df0aaef6a7fa2a90ea7b00b24466f95728
buoutuanzi <96377696+buoutuanzi@users.noreply.github.com>
2023-07-09 20:45:00 +0800

    编写修改子弹速度的Buff效果

--- Assets/Scripts/Buff/ChangeBulletVelocityBuff.cs
+++ Assets/Scripts/Buff/ChangeBulletVelocityBuff.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeBulletVelocityBuff : MonoBehaviour
+public class ChangeBulletVelocityBuff : IBuff
 {
-    // Start is called before the first frame update
-    void Start()
+    private float originShootForce = 0.0f;
+    private float curSpeedScale = 1;
+    private float minScale = 0.5f;
+    private float maxScale = 2;
+    private Shooter shooter = null;
+    const string shooterPath = "GameView/Shooter";
+    public void Destroy()
     {
-        
+        Reset();
+        shooter = null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Reset()
     {
-        
+        UpdateVelocityByScale(1 / curSpeedScale);
+    }
+
+    public void Trigger(BuffTriggerArgs args)
+    {
+        if (!shooter)
+        {
+            shooter = GameObject.Find(shooterPath).GetComponent<Shooter>();
+            originShootForce = shooter.defaultShootForce;
+        }
+
+        UpdateVelocityByScale((float)args.args);
+    }
+
+    private void UpdateVelocityByScale(float scale)
+    {
+        float oldScale = curSpeedScale;
+        curSpeedScale *= scale;
+        curSpeedScale = Mathf.Clamp(curSpeedScale, minScale, maxScale);
+
+        // 修改发射器的发射力度
+        if (shooter)
+        {
+            shooter.defaultShootForce = curSpeedScale * originShootForce;
+        }
+        // 修改已激活子弹的速度
+        float deltaScale = curSpeedScale / oldScale;
+        foreach (var bullet in BulletSpawn.Instance.activeBulletSet)
+        {
+            Rigidbody2D rigBullet = bullet.GetComponent<Rigidbody2D>();
+            rigBullet.velocity = rigBullet.velocity * deltaScale;
+        }
     }
 }
